04a050f963b08d4a8d2658640f6db68ca80ba010
Erin Lincoln <el77717@gmail.com>
2022-12-14 21:47:07 -0500

    Add socket stuff to backend

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+import socket
 
 app = Flask(__name__)
 CORS(app)
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 @app.route('/lights/', methods=['GET', 'POST'])
 def welcome():
     data = request.json
-    print(data['colors'])
     # zone = data.zone
     zone = 0
-    color = data.colors[0][1:]
-
-    print(color)
+    color = data['colors'][0][1:]
 
     HOST = "10.0.0.252"  # The server's hostname or IP address
     PORT = 1234  # The port used by the server
 
-    arr = [zone]
+    colorString = '0'+ str(zone)
 
     for x in range(100):
-        arr.append(color)
+        colorString += color
+
+    arr = bytearray.fromhex(colorString)
+
+    s.connect((HOST, PORT))
+    s.sendall(arr)
+    s.close()
+
+    return 'success'
 
 
 if __name__ == '__main__':
